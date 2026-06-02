ae0dd364cbfdb02027db959b184f71476d3d5a87
Sahil-Dixit05 <f20230395@hyderabad.bits-pilani.ac.in>
2026-06-02 10:59:37 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Sahil Dixit",
+    author_email="dixitsahil.125@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
